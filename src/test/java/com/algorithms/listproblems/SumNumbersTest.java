f18427b59c6b764cdb76a6da575d234e7f2fde60
anuragmuthyam <anu.drumcoder@gmail.com>
2020-12-04 10:28:09 -0500

    added tests for sum numbers

--- src/test/java/com/algorithms/listproblems/SumNumbersTest.java
+++ src/test/java/com/algorithms/listproblems/SumNumbersTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.Arrays;
 import java.util.List;
 
-import static com.algorithms.problems.listproblems.SumNumbers.sumNumbersImperApproach;
+import static com.algorithms.problems.listproblems.SumNumbers.*;
 
 public class SumNumbersTest {
 
@@ -14,5 +14,17 @@ public class SumNumbersTest {
     public void testSumImperApproach() {
         List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         Assertions.assertEquals(sumNumbersImperApproach(list), 55);
+    }
+
+    @Test
+    public void testSumFuncApproach1() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Assertions.assertEquals(sumNumbersFuncApproach1(list), 55);
+    }
+
+    @Test
+    public void testSumFuncApproach2() {
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        Assertions.assertEquals(sumNumbersFuncApproach2(list), 55);
     }
 }
