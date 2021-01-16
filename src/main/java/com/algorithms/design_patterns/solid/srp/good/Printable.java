9203fb58fbdf34cc8d71a29417d56f59949f704c
anuragmuthyam <anu.drumcoder@gmail.com>
2021-01-16 13:57:51 -0600

    removed desig patterns pkg

--- src/main/java/com/algorithms/design_patterns/solid/srp/good/Printable.java
+++
@@ -1,5 +0,0 @@
-package com.algorithms.design_patterns.solid.srp.good;
-
-public interface Printable {
-	String getContent();
-}
--- src/main/java/com/algorithms/design_patterns/solid/srp/good/Printer.java
+++
@@ -1,8 +0,0 @@
-package com.algorithms.design_patterns.solid.srp.good;
-
-public class Printer {
-	public void print(Printable printable) {
-		System.out.println(printable.getContent());
-	}
-	
-}
